1a7670357c20153c056fb0ebca21fc668f184398
Nikolay Khabarov <2xl@mail.ru>
2017-05-14 03:10:34 +0300

    update tests

--- runtests.sh
+++ runtests.sh
@@ -11,12 +11,10 @@ echo '* run with RPi board with connected to pin GPIO21 LED. LED should    *'
 echo '* light up on pullup, set, and DMA test events.                      *'
 echo '**********************************************************************'
 echo '---------------------------Unit tests---------------------------------'
-pypy -m unittest discover "$@" --pattern="test_*.py"
+python -m unittest discover "$@" --pattern="test_*.py"
 echo '-----------------------Integration tests------------------------------'
-sudo pip install .
 res="$(pycnc tests/rects.gcode 2>&1)"
 res="$res$(pycnc tests/test_parser.gcode 2>&1)"
-sudo pip uninstall -y pycnc
 if echo "$res" | grep -q -i error; then
   echo "$res"
   exit 1
