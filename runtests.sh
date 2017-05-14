4f0b4d361d55916de7a0a169daae67e935b16e8c
Nikolay Khabarov <2xl@mail.ru>
2017-05-14 03:05:40 +0300

    add integration test

--- runtests.sh
+++ runtests.sh
@@ -10,4 +10,15 @@ echo '* example Raspberry Pi tests is tests/rpgpio_test.sh which should be *'
 echo '* run with RPi board with connected to pin GPIO21 LED. LED should    *'
 echo '* light up on pullup, set, and DMA test events.                      *'
 echo '**********************************************************************'
-python -m unittest discover "$@" --pattern="test_*.py"
+echo '---------------------------Unit tests---------------------------------'
+pypy -m unittest discover "$@" --pattern="test_*.py"
+echo '-----------------------Integration tests------------------------------'
+sudo pip install .
+res="$(pycnc tests/rects.gcode 2>&1)"
+res="$res$(pycnc tests/test_parser.gcode 2>&1)"
+sudo pip uninstall -y pycnc
+if echo "$res" | grep -q -i error; then
+  echo "$res"
+  exit 1
+fi
+
